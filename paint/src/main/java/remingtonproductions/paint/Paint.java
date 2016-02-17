e305915faef2647469a04e9e93955eb325ee056d
miro.glamuzina <miro.glamuzina@gmail.com>
2016-02-17 13:20:30 -0800

    Initial Commit of Paint/Drawing Activity module.

---
+++ paint/src/main/java/remingtonproductions/paint/Paint.java
@@ -0,0 +1,13 @@
+package remingtonproductions.paint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Paint extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+    }
+}
